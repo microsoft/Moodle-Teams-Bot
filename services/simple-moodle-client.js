54c4a4ba0250e5342cef34ddfb97a235c6617b6f
Nilesh Khaitan <nkhaitan@bu.edu>
2019-01-14 17:24:05 -0800

    Commit latest bot code

--- services/simple-moodle-client.js
+++ services/simple-moodle-client.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 let moodleClient = require("moodle-client");
 let rp  = require("request-promise");
 let promiseRetry = require("promise-retry");
